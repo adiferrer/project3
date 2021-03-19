6f1e887903006a2d158f7d66ae3976c2f63f69a6
adi <adi@fpcph-mbp.local>
2021-03-19 19:42:42 +0800

    some files

---
+++ src/CurriculumData.java
@@ -0,0 +1,8 @@
+public class CurriculumData {
+    /** Data Fields */
+    private int year;
+    private int sem;
+    private String courseNumber;
+    private String descriptiveTitle;
+    private float units;
+}
